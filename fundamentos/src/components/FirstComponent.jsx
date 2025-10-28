10a0a5c03d1540bc03ff895c92ee24b6527dba8d
andre10dias <andre10dias@ymail.com>
2025-10-28 08:23:17 -0300

    Formas de comentar o código

--- fundamentos/src/components/FirstComponent.jsx
+++ fundamentos/src/components/FirstComponent.jsx
@@ -1,6 +1,13 @@
+// Importação do React
+
 const FirstComponent = () => {
+    // JSX que define a estrutura do componente
+    /*
+        Comentário multilinha
+    */
     return (
         <div>
+            {/* JSX que define o conteúdo do componente */}
             <h1>This is the First Component</h1>
         </div>
     );
