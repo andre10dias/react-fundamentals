b32b350dea7ef79a49c5a82e3be05726b754a94a
andre10dias <andre10dias@ymail.com>
2025-10-29 09:00:18 -0300

    Imagens

--- 2_avancando/src/App.jsx
+++ 2_avancando/src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
 
+/**
+ * Importa como se fosse um componente ou módulo;
+ * O src é dinâmico, ou seja, o Webpack faz o build e gera
+ * o caminho correto para a imagem na aplicação final.
+ */
+import City from './assets/city.jpg'
+
 function App() {
   return (
     <>
       <h1>Avançando em React</h1>
+      {/* Imagem na pasta public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem na pasta assets */}
+      <div>
+        <img src={City} alt="Cidade" />
+      </div>
     </>
   )
 }
