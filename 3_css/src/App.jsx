eb242f613e23a1ee9a7e7961f56fd0b5e6a5cd22
andre10dias <andre10dias@ymail.com>
2025-11-04 09:22:05 -0300

    CSS de componente

--- 3_css/src/App.jsx
+++ 3_css/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import MyComponent from './component/MyComponent'
 
 function App() {
   return (
     <>
+      {/* css global */}
       <h1>React com CSS</h1>
+      {/* css de componente */}
+      <MyComponent />
+      <p>Este é um parágrafo do App.js</p>
     </>
   )
 }
