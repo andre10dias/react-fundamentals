43ef2b8464e3ad7bc16d5832ecbe075e2adf16b5
andre10dias <andre10dias@ymail.com>
2025-11-09 08:55:43 -0300

    Label envolvendo input

--- 4_form/src/components/MyForm.jsx
+++ 4_form/src/components/MyForm.jsx
@@ -4,10 +4,17 @@ const MyForm = () => {
   return (
     <div>
         <form>
+            {/* criando fomulário */}
             <div>
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" id="name" placeholder='Digite o seu nome' />
             </div>
+
+            {/* label envolvendo input */}
+            <label>
+                <span>E-mail:</span>
+                <input type="email" name="email" placeholder="Digite o seu email" />
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>
